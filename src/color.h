776119254213d398ba8b7f3505728e2b19dcb099
Kalp Shah <shahkalp07@gmail.com>
2026-05-10 17:36:16 +0530

    Added Gamma Correction

--- src/color.h
+++ src/color.h
@@ -6,10 +6,23 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream &out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply Gamma Correction (Gamma = 2.0)
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Converting [0, 1] component values to byte range [0, 255]
     static const interval intensity(0.0, 0.999);
